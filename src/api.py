91a217ccd5d6fa271c39a58c61ea2f07f7513af9
ankurbohra04 <ankurbohra04@gmail.com>
2021-07-23 21:00:34 +0530

    format document

--- src/api.py
+++ src/api.py
@@ -1,3 +1,5 @@
+import httplib2
+import logging
 import os
 from typing import Sequence, Optional, List, Dict, Type, Union
 
@@ -16,6 +18,7 @@ scopes = [
     "openid"
 ]
 
+
 def get_creds(scopes: Sequence[str], data_folder: str = "data",
               show_auth_prompt: bool = True, reuse_creds: bool = True) -> Type[Credentials]:
     """Get/create user credentials in given folder with specified scopes.
@@ -24,16 +27,17 @@ def get_creds(scopes: Sequence[str], data_folder: str = "data",
         data_folder: The folder containing client_secret.json and to store credentials in.
         show_auth_prompt: Whether or not to show the user the authourization link in the console.
         reuse_creds: Whether or not to use credentials from previous runs.
-    
+
     Returns:
         The credentials stored or created.
-    """    
+    """
     creds: Optional[Type[Credentials]] = None
     # The file token.json stores the user"s access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
     if reuse_creds and os.path.exists(data_folder+"\\token.json"):
-        creds = Credentials.from_authorized_user_file(data_folder+"\\token.json", scopes)
+        creds = Credentials.from_authorized_user_file(
+            data_folder+"\\token.json", scopes)
 
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -45,44 +49,53 @@ def get_creds(scopes: Sequence[str], data_folder: str = "data",
             if show_auth_prompt:
                 creds = flow.run_local_server(port=0)
             else:
-                creds = flow.run_local_server(port=0, authorization_prompt_message="")
+                creds = flow.run_local_server(
+                    port=0, authorization_prompt_message="")
         # Save the credentials for the next run
         with open(data_folder+"\\token.json", "w") as token:
             token.write(creds.to_json())
     return creds
 
+
 # service will be built only once per run
 service = None
+
+
 def get_service(reuse_creds: bool = True) -> Resource:
     '''Construct or return a service for interacting with the Calendar v3 API
     Args:
         reuse_creds: Whether or not to use credentials from previous runs.
-    
+
     Returns:
         A Resource object that can interact with the Calendar v3 API
     '''
     global service, scopes
     if service is None:
-        credentials: Credentials = get_creds(scopes, data_folder="data", show_auth_prompt=False, reuse_creds=reuse_creds)
+        credentials: Credentials = get_creds(
+            scopes, data_folder="data", show_auth_prompt=False, reuse_creds=reuse_creds)
         service = build("calendar", "v3", credentials=credentials)
     return service
 
 # Separate function because this will be needed in the selection UI
+
+
 def get_calendar_list() -> Resource:
     '''Get a calendar list's items with the first 100 calendars.
-    
+
     Returns:
         A list of calendarListEntrys.
     '''
     service = get_service()
-    calendar_list = service.calendarList().list().execute() # Default maxResults = 100 which is sufficient
+    # Default maxResults = 100 which is sufficient
+    calendar_list = service.calendarList().list().execute()
     return calendar_list["items"]
+
 
 def get_calendar_from_name(calendar_name: str) -> Optional[Dict]:
     '''Get the calendar associated with a given calendar name.
     Args:
         calendar_name: The name of the calendar.
-    
+
     Returns:
         The calendar associated with the calendar name.
     '''
@@ -93,6 +106,7 @@ def get_calendar_from_name(calendar_name: str) -> Optional[Dict]:
             calendar_id = calendar["id"]
             calendar = service.calendars().get(calendarId=calendar_id).execute()
             return calendar
+
 
 def get_events_in_time_span(calendar_id: str, time_from: datetime, time_to: datetime,
                             allow_incomplete_overlaps: bool = False, filters: List[str] = ["+Inside", "+OverStart", "+OverEnd", "+Across"]) -> List[Dict]:
@@ -109,7 +123,7 @@ def get_events_in_time_span(calendar_id: str, time_from: datetime, time_to: date
             Examples:
                 ["+Inside", "+OverStart"] will filter in only Inside and Overstart type events.
                 ["+Inside", "-OverEnd"] will filter in only Inside type events. Note that the -OverEnd filter is redundant here.
-    
+
     Returns:
         A list of Events each with an added field "overlapType" of possible values:
             "Inside": The Event starts and ends inside the time span.
@@ -127,7 +141,8 @@ def get_events_in_time_span(calendar_id: str, time_from: datetime, time_to: date
     events_overlapping_in_span = events.list(
         calendarId=calendar_id,
         timeMin=time_from.isoformat(), timeMax=time_to.isoformat(),
-        singleEvents=True, orderBy="startTime" # startTime order requires singleEvents to be True
+        # startTime order requires singleEvents to be True
+        singleEvents=True, orderBy="startTime"
     ).execute()["items"]
     events_chosen = []  # "Sink" for events passing filter
     for event in events_overlapping_in_span:
@@ -157,7 +172,7 @@ def get_events_in_time_span(calendar_id: str, time_from: datetime, time_to: date
         # else:
             # No overlapType assigned as only Inside is allowed and event wasn't inside
 
-        if "overlapType" in event: # Event passed appropriate filter
+        if "overlapType" in event:  # Event passed appropriate filter
             events_chosen.append(event)
 
     type_filters = {}
@@ -179,13 +194,13 @@ def get_events_in_time_span(calendar_id: str, time_from: datetime, time_to: date
         
     return events_passed
 
-def get_events_starting_from_now(calendar_id: Union[str, List[str]], range_offset: timedelta = timedelta(minutes=1),) ->  List[Dict]:
+def get_events_starting_from_now(calendar_id: Union[str, List[str]], range_offset: timedelta = timedelta(minutes=1),) -> List[Dict]:
     '''Get events starting from the time the function is called upto an offset.
 
     Args:
         calendar_id: calendarId of the calendar to search.
         range_offset: The offset from the time the function is called, before which the event must start.
-    
+
     Returns:
         The events starting in the range [now, now + range_offset], with an added field "overlapType" of
         possible values:
@@ -195,7 +210,8 @@ def get_events_starting_from_now(calendar_id: Union[str, List[str]], range_offse
     time_from = datetime.now().astimezone()
     time_to = time_from + range_offset
 
-    events = get_events_in_time_span(calendar_id, time_from, time_to, allow_incomplete_overlaps=True, filter=["+Inside", "+OverEnd"])
+    events = get_events_in_time_span(
+        calendar_id, time_from, time_to, allow_incomplete_overlaps=True, filters=["+Inside", "+OverEnd"])
     return events
 
 def get_user_info(credentials):
