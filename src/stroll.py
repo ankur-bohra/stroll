d2927f84e77bd3b961e15b55456cc302975c02ff
ankurbohra04 <ankurbohra04@gmail.com>
2021-07-23 21:03:14 +0530

    switch from infi.systray to pystray pystray allows dynamically changing the tray menu, and it comes with a basic notification implementation

--- src/stroll.py
+++ src/stroll.py
@@ -1,29 +1,34 @@
 import os
 import re
-import toml
 from datetime import datetime, timedelta
-
-from infi.systray import SysTrayIcon
+from PIL import Image
+from pystray import Icon, Menu, MenuItem as Item
 
 import api
+from data import Settings, SessionData
 from scheduler import Scheduler
 
-ICON = "images/stroll.ico"
+ICON = Image.open("images\\stroll.ico")
 SCOPES = [
     "https://www.googleapis.com/auth/calendar.readonly",
     "https://www.googleapis.com/auth/calendar.events.readonly",
-    "https://www.googleapis.com/auth/calendar.events"
+    "https://www.googleapis.com/auth/calendar.events",
+    "https://www.googleapis.com/auth/userinfo.email",
+    "openid"
 ]
 REGEXP = r'(\d+)\?pwd=(\w+)'
 
-settings = {}
-
 scheduler = Scheduler()
+settings = Settings()
+data = SessionData()
 
 def link_account(sysTrayIcon):
-    global SCOPES
-    api.get_creds(SCOPES, show_auth_prompt=False, reuse_creds=False)
-    auto_sync()
+    creds = api.get_creds(SCOPES, show_auth_prompt=False, reuse_creds=False)
+    user_info = api.get_user_info(creds)
+    if user_info:
+        data.set("email", user_info.get("email"))
+        sysTrayIcon.update_menu()
+    auto_sync() 
 
 def get_next_event():
     global next_event
@@ -32,14 +37,16 @@ def get_next_event():
     calendar_list = api.get_calendar_list()
     for calendar in calendar_list:
         calendar_id = calendar.get("id")
+
         possible_events = api.get_events_starting_from_now(calendar_id, range_offset=timedelta(days=1))  # Random offset
-        # Store only zoom link containing events
-        for event in possible_events:
-            if bool(re.search(REGEXP, event.get("description"))):
+        # Store only zoom link containing events 
+        for event in possible_events:            
+            if event.get("description") and bool(re.search(REGEXP, event.get("description"))):
                 events.append(event)
     
     # NOTE: Timezone conversion isn't required here (constant offset)
     events.sort(key = lambda event: datetime.fromisoformat(event.get("start").get("dateTime")), reverse=True)
+    print([(event.get("summary"), event.get("start").get("dateTime"), event.get("overlapType")) for event in events])
     return len(events) > 0 and events.pop() or None
 
 def join_event(event):
@@ -47,48 +54,37 @@ def join_event(event):
     description = event.get("description")
     pattern = re.search(REGEXP, description)
     url = f"zoommtg://zoom.us/join?action=join&confno={pattern.group(1)}&pwd={pattern.group(2)}"
-    command = f"{settings.get('Joining').get('zoom-path')} --url=\"{url}\""
+    command = f"{settings.get('Joining.zoom-path')} --url=\"{url}\""
     os.popen(command)
 
-def join_next_event(sysTrayIcon):
+def join_next_event():
     event = get_next_event()
     if event:
         join_event(event)
 
 def schedule_next_event():
-    global settings
+    global settings, tray_icon
     event = get_next_event()
     if event:
-        startTime = datetime.fromisoformat(event.get("start").get("dateTime"))
-        joinTime = startTime - timedelta(seconds=settings.get("Joining").get("offset"))
-        scheduler.add_task(joinTime, lambda: join_event(event))
-
-def load_settings():
-    global settings
-    settings = toml.load("settings.toml")
-    auto_sync()  # Just in case some joining/syncing settings have been changed
-
-def modify_settings(field, value):
-    global settings
-    keys = field.split(".")[::-1]
-    cursor = settings
-    while len(keys) > 1:  # The last part is the actual key, the one before that is the last dictionary
-        key = keys.pop()
-        cursor = cursor.get(key)
-    cursor[keys.pop()] = value
-    with open("settings.toml", "w") as settings_file:
-        toml.dump(settings, settings_file)
-    load_settings()  # Handle all updates
-
-def on_quit(sysTrayIcon):
-    global scheduler
-    scheduler.terminate()
+        if not scheduler.head or event.get("id") != scheduler.head.value.get("id"):
+            # This is a new event, remove the old event
+            scheduler.clear()
+            # Schedule the new event
+            startTime = datetime.fromisoformat(event.get("start").get("dateTime"))
+            joinTime = startTime - timedelta(seconds=settings.get("Joining.offset"))
+            scheduler.add_task(joinTime, lambda: join_event(event))
+            # Tell the user a new event has been scheduled
+            name = event.get("summary")
+            tray_icon.notify(f"Event \"[{name}]\" has been scheduled for {joinTime.strftime('%r')}", "New Event Scheduled")
+    elif scheduler.head:
+        # The event must have been removed or completed
+        scheduler.clear()
 
 current_sync_origin = datetime.now()
 def auto_sync(sync_origin=None):
     global scheduler, current_sync_origin, settings
-
-    if settings.get("Joining").get("auto-join") == False:
+    # Terminate if auto-join is disabled or the scheduler is terminated
+    if not settings.get("Joining.auto-join") or not scheduler.active:
         return
 
     now = datetime.now().astimezone()
@@ -103,36 +99,74 @@ def auto_sync(sync_origin=None):
     # All sync issues resolved, proceed to actual syncing
     # Don't interact with api if no credentials are present at all
     if os.path.exists("data/token.json"):
-        scheduler.clear()
         schedule_next_event()
-        next_call = now + timedelta(seconds=settings.get("Syncing").get("period"))
+        next_call = now + timedelta(seconds=settings.get("Syncing.period"))
         scheduler.add_task(next_call, lambda: auto_sync(sync_origin))
+
+menu_items = []
+def get_menu_items():
+    global menu_items, settings, data
+    menu_items = []
+
+    # First comes the current account if present
+    email = data.get("email")
+    if email:
+        menu_items.append(Item(
+            email, lambda tray_icon: tray_icon, enabled=False
+        ))
+    
+    menu_items.append(Item("Link New Account", link_account))
+
+    menu_items.append(Menu.SEPARATOR)
+    menu_items.append(Item("Join Next Event", lambda tray_icon: join_next_event()))
+    menu_items.append(Item("Sync Next Event", lambda tray_icon: auto_sync()))
+
+    menu_items.append(Menu.SEPARATOR)
+    menu_items.append(Item("Open Settings File", lambda tray_icon: os.popen(f"{settings.get('Settings.path')}")))
+    menu_items.append(Item("Load Settings", lambda tray_icon: settings.load()))
+
+    menu_items.append(Menu.SEPARATOR)
+    menu_items.append(Item(
+        "Auto-Join",
+        lambda tray_icon, item: settings.set("Joining.auto-join", not item.checked),
+        checked=lambda item: settings.get("Joining.auto-join")
+    ))
+
+    menu_items.append(Menu.SEPARATOR)
+    menu_items.append(Item("Quit", stop))
+    return menu_items
+
 
 def stop_joining():
     global current_sync_origin, scheduler
-    current_sync_origin = datetime.now().astimezone()
-    scheduler.clear()  # Clear previously scheduled events
+    # Terminate current sync loop
+    current_sync_origin = datetime.now().astimezone()  # Fakes a new sync loop
+    scheduler.clear()  # Clear scheduled events
 
-DEFAULT_INDEX = 1  # Join next event when tray icon is double clicked
-menu = (
-    ("Link New Account", None, link_account),
-    ("<SEPARATOR>", None, lambda i: None),
-    ("Sync Next Event", None, auto_sync),
-    ("Join Next Event", None, join_next_event),
-    ("<SEPARATOR>", None,  lambda i: None),
-    ("Auto-Joining", None, (
-        ("Enable Joining", None, lambda i: modify_settings("Joining.auto-join", True)),
-        ("Disable Joining", None, lambda i: modify_settings("Joining.auto-join", False))
-    )),
-    ("Settings", None, (
-        ("Open Settings File", None, lambda i: os.popen(os.path.join(os.getcwd(), "settings.toml"))),
-        ("Reload Settings", None, load_settings)
-    ))
-)
+def init(tray_icon):
+    global scheduler, data, settings, data
+    # Indicate startup
+    tray_icon.visible = True
+    tray_icon.notify("Stroll is starting up")
 
+    # Handle data files
+    settings.load()
+    data.load()
 
-scheduler.start()
-load_settings()  # Also starts the initial sync loop
-os.popen(settings.get("Joining").get("zoom-path"))  # Zoom start-up and login shouldn't affect prejoin period
-systray = SysTrayIcon(ICON, "Stroll", menu, on_quit, DEFAULT_INDEX)
-systray.start()
+    # Initialize first sync loop
+    scheduler.start()
+    auto_sync()
+    tray_icon.remove_notification()
+
+def stop(tray_icon):
+    global scheduler, data, settings, data
+    data.dump()
+    settings.dump()
+    if scheduler.active:
+        scheduler.terminate()
+    tray_icon.stop()
+    
+os.popen(settings.get("Joining.zoom-path"))  # Zoom start-up and login shouldn't affect prejoin period
+tray_menu = Menu(*get_menu_items())
+tray_icon = Icon("Stroll", ICON, menu=tray_menu)
+tray_icon.run(init)
