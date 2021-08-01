17bfba35368f3b1a432b714744236515b5476d2e
ankurbohra04 <ankurbohra04@gmail.com>
2021-08-01 13:51:18 +0530

    rename non-constant variable

--- src/util/path.py
+++ src/util/path.py
@@ -12,11 +12,11 @@ root = os.path.dirname(__file__)
 # Get the path of the project root folder
 # In the built version, program is compiled to a location like this: \\dist\\stroll\\util\\path.pyc => DEPTH = 1
 # In the source version, the file is inside util inside of src => DEPTH=2
-THIS_DIR_DEPTH = 1 if is_built else 2
-while THIS_DIR_DEPTH > 0:
+depth_from_root = 1 if is_built else 2
+while depth_from_root > 0:
     # Move one level up
     root = os.path.dirname(root)
-    THIS_DIR_DEPTH -= 1  # Decrement the depth
+    depth_from_root -= 1  # Decrement the depth
 
 
 def from_root(path):
